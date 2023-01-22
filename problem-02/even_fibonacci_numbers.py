1391eac0c975d854f704df663ef51d586155f563
marianac-campos <marianacristinadecampos@gmail.com>
2023-01-22 08:08:19 -0300

    refactoring the code

--- problem-02/even_fibonacci_numbers.py
+++ problem-02/even_fibonacci_numbers.py
@@ -2,10 +2,11 @@ limit = 4000000
 sum = 0
 a = 1
 b = 1
-while b < limit:
+c = a+b
+while c < limit:
   
-  if b % 2 == 0 : sum += b
-  h = a + b
-  a, b = b, h
-  
+  sum += c
+  a = b + c
+  b = c + a
+  c = a + b
 print(sum)
